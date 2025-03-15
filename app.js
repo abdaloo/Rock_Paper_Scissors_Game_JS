ff69f5a4a9583c2e00c8254572a399ad2f177325
Abdal Hussain <abdalhussain5790@gmail.com>
2025-03-15 20:01:11 +0500

    Complete first Step

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(){
-
+    let result = Math.floor(Math.random()*3 + 1);
+    if(result === 1){
+        return "rock";
+    } else if(result === 2){
+        return "paper";
+    } else{
+        return "scissors";
+    }
+    
 }
+
+console.log(getComputerChoice());
+
 
