67644981e34fa83ba9ab81ad1c8faa0ff6cbb354
Abdal Hussain <abdalhussain5790@gmail.com>
2025-03-29 20:03:38 +0500

    Convert if statment to switch statement

--- app.js
+++ app.js
@@ -5,38 +5,38 @@ let computerScore = 0;
 // Step 1:
 function getComputerChoice(){
     let result = Math.floor(Math.random()*3 + 1);
-    if(result === 1){
-        return "rock";
-    } else if(result === 2){
-        return "paper";
-    } else{
-        return "scissors";
+    switch(result){
+        case 1:
+             return "rock";
+        case 2:
+            return "paper";
+        default:
+            return "scissors";
     }
-    
 }
 
-// console.log(getComputerChoice()); // this is just for text the function
 
 // Step 2:
 function getHumanChoice(){
-    let result = +prompt("Enter a number i.e.[1 = rock, 2 = paper, 3 = scissors]","");
+    let result = Number(prompt("Enter a number i.e.[1 = rock, 2 = paper, 3 = scissors]",""));
     if(result === 1){
-        return "rock";
+        return console.log("rock");
     } else if(result === 2){
-        return "paper";
+        return console.log("paper");
     } else if(result === 3){
-        return "scissors";
+        return console.log("scissors");
     } else{
-        return alert("Please enter the 1,2 or 3 number.");
+        return console.log("Please enter the 1,2 or 3 number.");
     }
 }
 
-// console.log(getHumanChoice()); // this is just for text the function
+console.log(getHumanChoice()); // this is just for test the function
+console.log(getComputerChoice()); // this is just for test the function
 
 //Step 4:
-function playRound(humanChoice,computerChoice){
+// function playRound(humanChoice,computerChoice){
 
-}
+// }
 
 
 
